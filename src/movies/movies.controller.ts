409dbade6729a386bf156b3441e6f8ec2b83f4cc
jhwmon <jmonaco88@bbangyanetworks.com>
2023-03-22 18:59:23 +0900

    first commit

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Param,
+  Query,
+  Body,
+  Get,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie make after: ${searchingYear}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number) {
+    this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
